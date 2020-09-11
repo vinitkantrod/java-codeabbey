c4f0193eb8c8abd397dd9a775e4a86ee4b197e4d
vinitkantrod <vinit@bounceshare.com>
2020-09-11 19:16:38 +0530

    added sum A plus B

---
+++ SumAPlusB.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class SumAPlusB {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Integer a = in.nextInt();
+        Integer b = in.nextInt();
+        System.out.println(String.valueOf(a + b));
+    }
+}
